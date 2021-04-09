a432ace6113a66ae242de549b46c02a7db95f3ed
dima-frost <frostdimonlex@gmail.com>
2021-04-09 16:17:23 +0300

    edit dockerfiles

---
+++ apache/script.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "My name is ${NAME} and my status is ${STATUS} ." >> index.html
+httpd-foreground
--- nginx/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm index.html
-touch index.html
-echo "My name is ${NAME} and I am ${AGE} years old." > index.html
